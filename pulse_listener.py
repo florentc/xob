fe90aa6c262c8e0ffc2aa0f56be48676dd0c7192
Tiger Sachse <tgsachse@gmail.com>
2019-08-18 22:23:49 -0700

    Remove unneeded code.

--- pulse_listener.py
+++ pulse_listener.py
@@ -7,7 +7,6 @@ Lightly edited by Tiger Sachse.
 import pulsectl
 import sys
 
-SINK_ID = 0
 
 def create_handle_event_function(sink_id):
     """Create a handle_event() function with a given sink ID."""
@@ -18,32 +17,33 @@ def create_handle_event_function(sink_id):
 
     return handle_event
 
+
 def get_current_volume(pulse_controller, sink_id):
     """Get the current volume to the nearest integer."""
     return round(pulse_controller.sink_list()[sink_id].volume.value_flat * 100)
 
+
+def is_muted(pulse_controller, sink_id):
+    """Determine if the sink is muted."""
+    return pulse_controller.sink_list()[sink_id].mute == 1
+
+
 # Start of the program.
+SINK_ID = 0
 with pulsectl.Pulse() as pulse_controller:
     try:
         pulse_controller.event_mask_set("sink")
         pulse_controller.event_callback_set(
             create_handle_event_function(SINK_ID),
         )
-        current_volume = get_current_volume(pulse_controller, SINK_ID)
-        currently_muted = pulse_controller.sink_list()[SINK_ID].mute == 1
 
         while True:
             pulse_controller.event_listen()
-            new_volume = get_current_volume(pulse_controller, SINK_ID)
-            newly_muted = pulse_controller.sink_list()[SINK_ID].mute == 1
-            if new_volume != current_volume or newly_muted != currently_muted:
-                print(new_volume, end="")
-                if newly_muted:
-                    print("!")
-                else:
-                    print("")
-                current_volume = new_volume
-                currently_muted = newly_muted
+            print(get_current_volume(pulse_controller, SINK_ID), end="")
+            if is_muted(pulse_controller, SINK_ID):
+                print("!")
+            else:
+                print("")
             sys.stdout.flush()
     except (pulsectl.PulseLoopStop, KeyboardInterrupt):
         pass
