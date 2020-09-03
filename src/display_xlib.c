aade2a0362428b66a95615391ff74fa82013a974
ohattsh <Hattshire@users.noreply.github.com>
2020-09-03 17:10:34 -0400

    Add missing license notices

--- src/display_xlib.c
+++ src/display_xlib.c
@@ -1,3 +1,20 @@
+/* xob - A lightweight overlay volume/anything bar for the X Window System.
+ * Copyright (C) 2020 Florent Ch.
+ *
+ * xob is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * xob is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with xob.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 #include "display.h"
 #include <X11/Xlib.h>
 
